e09e5cc34208aa28321ef2bc5886ce40059f4071
Ellis Adigvom <ellisadigvom@gmail.com>
2019-05-30 11:58:24 +0000

    fix webhook, clean up plugin

--- icii/icii.py
+++ icii/icii.py
@@ -6,67 +6,13 @@ class ICII(BotPlugin):
     icii notifications
     """
 
-    def activate(self):
-        """
-        Triggers on plugin activation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(ICII, self).activate()
-
-    def deactivate(self):
-        """
-        Triggers on plugin deactivation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(ICII, self).deactivate()
-
-    # def get_configuration_template(self):
-    #     """
-    #     Defines the configuration structure this plugin supports
-
-    #     You should delete it if your plugin doesn't use any configuration like this
-    #     """
-    #     return {"EXAMPLE_KEY_1": "Example value", "EXAMPLE_KEY_2": ["Example", "Value"]}
-
-    # def check_configuration(self, configuration):
-    #     """
-    #     Triggers when the configuration is checked, shortly before activation
-
-    #     Raise a errbot.ValidationException in case of an error
-
-    #     You should delete it if you're not using it to override any default behaviour
-    #     """
-    #     super(ICII, self).check_configuration(configuration)
-
-    # def callback_connect(self):
-    #     """
-    #     Triggers when bot is connected
-
-    #     You should delete it if you're not using it to override any default behaviour
-    #     """
-    #     pass
-
-    # def callback_message(self, message):
-    #     """
-    #     Triggered for every received message that isn't coming from the bot itself
-
-    #     You should delete it if you're not using it to override any default behaviour
-    #     """
-    #     pass
-
-    # def callback_botmessage(self, message):
-    #     """
-    #     Triggered for every message that comes from the bot itself
-
-    #     You should delete it if you're not using it to override any default behaviour
-    #     """
-    #     pass
-
     @webhook
     def healthz(self, incoming_request):
         return "healthz"
+
+    @webhook
+    def message(self, req):
+        self.send(self.build_identifier("#icam_corp_portal"), req["message"])
 
     # Passing split_args_with=None will cause arguments to be split on any kind
     # of whitespace, just like Python's split() does
@@ -74,17 +20,3 @@ class ICII(BotPlugin):
     def example(self, message, args):
         """A command which simply returns 'Example'"""
         return "Example"
-
-    @arg_botcmd("name", type=str)
-    @arg_botcmd("--favorite-number", type=int, unpack_args=False)
-    def hello(self, message, args):
-        """
-        A command which says hello to someone.
-
-        If you include --favorite-number, it will also tell you their
-        favorite number.
-        """
-        if args.favorite_number is None:
-            return f"Hello {args.name}."
-        else:
-            return f"Hello {args.name}, I hear your favorite number is {args.favorite_number}."
