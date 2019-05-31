e5c2beb66dea79c7cf111a5c9bb300c16e779a9a
Ellis Adigvom <ellisadigvom@gmail.com>
2019-05-31 13:14:58 +0000

    add a command to try out sending a card

--- icii/icii.py
+++ icii/icii.py
@@ -20,3 +20,17 @@ class ICII(BotPlugin):
     def example(self, message, args):
         """A command which simply returns 'Example'"""
         return "Example"
+
+    @botcmd()
+    def hello_card(self, msg, args):
+        """Say a card in the chatroom."""
+        self.send_card(
+            title="Title + Body",
+            body="text body to put in the card",
+            # thumbnail='https://raw.githubusercontent.com/errbotio/errbot/master/docs/_static/errbot.png',
+            # image='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+            # link="http://www.google.com",
+            fields=(("First Key", "Value1"), ("Second Key", "Value2")),
+            color="red",
+            in_reply_to=msg,
+        )
